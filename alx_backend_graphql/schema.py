7580d619745bce9979afa96b6064e1f116233e33
Fadel <afadeilou@gmail.com>
2025-08-14 10:44:18 +0100

    correction

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,14 +1,11 @@
 import graphene
-from graphene_django import DjangoObjectType
-import crm.schema
+from crm.schema import Query as CRMQuery
+from crm.schema import Mutation as CRMMutation
 
-
-class Query(crm.schema.Query, graphene.ObjectType):
+class Query(CRMQuery, graphene.ObjectType):
     pass
 
-
-class Mutation(crm.schema.Mutation, graphene.ObjectType):
+class Mutation(CRMMutation, graphene.ObjectType):
     pass
-
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
